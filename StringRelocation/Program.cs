b1de16b1b62b18e7f503eb741123e71ce300a503
Lawrence Tan <lawrence-916@hotmail.com>
2017-08-25 11:20:49 +1000

    update

---
+++ StringRelocation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRelocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a string: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Please Enter an index: ");
+            int nInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{1}",userInput.Substring(userInput.Length - nInput),userInput.Substring(0,userInput.Length-nInput));
+            Console.ReadLine();
+
+        }
+    }
+}
